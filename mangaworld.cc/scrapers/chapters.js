77f6f2a9fd1d118d2be9ad8ffedad743faecf0cb
Giorgio Bertolotti <giorgiobertol@gmail.com>
2021-02-17 20:33:23 +0100

    :tada: Init chapters scraper script

--- mangaworld.cc/scrapers/chapters.js
+++ mangaworld.cc/scrapers/chapters.js
@@ -2,14 +2,17 @@ function Header(key, value) {
   this.key = key;
   this.value = value;
 }
+
 function ExtraInfo(value) {
   this.value = value;
 }
+
 function Output(link, linkIdentifier, moduleID, isDecodable, headers) {
   this.link = link;
   this.moduleID = moduleID;
   this.headers = headers;
 }
+
 function ChaptersObject(
   request,
   method,
@@ -27,19 +30,45 @@ function ChaptersObject(
   this.javaScript = javaScript;
   this.output = output;
 }
-var savedData = document.getElementById("katsu-final-data");
-var parsedJson = JSON.parse(savedData.innerHTML);
-var moduleID = "9809234289";
-var headers = [new Header("Referer", parsedJson.request)];
-var extraInfo = [new ExtraInfo("")];
-var chaptersObject;
-var output = [];
-var images = document.querySelectorAll(".page-break.no-gaps > img");
-for (var x = 0; x < images.length; x++) {
-  var image = images[x].getAttribute("data-src");
-  var imageLink = image.trim();
-  output.push(new Output(imageLink, "", moduleID, "false", headers));
+
+const savedData = document.getElementById('katsu-final-data');
+const parsedJson = JSON.parse(savedData.innerHTML);
+const moduleID = '199809071';
+const headers = [new Header('Referer', parsedJson.request)];
+const extraInfo = [new ExtraInfo('')];
+const output = [];
+
+const pNodes = document.querySelectorAll('p');
+const lastP = pNodes[pNodes.length - 1];
+const mangaworldScript = lastP.innerText;
+
+const firstImage = document
+  .querySelector('#page > img')
+  .getAttribute('src')
+  .trim();
+const imageBaseLink = firstImage.substring(0, firstImage.lastIndexOf('/') + 1);
+
+const fromPages = mangaworldScript.substring(
+  mangaworldScript.indexOf('\"pages\":[')
+);
+const pagesRaw = fromPages.substring(0, fromPages.indexOf(']'));
+const pagesStr = pagesRaw.split(',');
+
+for (let x = 1; x < pagesStr.length; x++) {
+  const pageRoute = pagesStr[x].replaceAll('\"', '');
+  const image = `${imageBaseLink}${pageRoute}`;
+  output.push(new Output(image, '', moduleID, 'false', headers));
 }
-chaptersObject = new ChaptersObject("", "", headers, extraInfo, "", "", output);
-var finalJson = JSON.stringify(chaptersObject);
+
+const chaptersObject = new ChaptersObject(
+  '',
+  '',
+  headers,
+  extraInfo,
+  '',
+  '',
+  output
+);
+
+const finalJson = JSON.stringify(chaptersObject);
 savedData.innerHTML = finalJson;
